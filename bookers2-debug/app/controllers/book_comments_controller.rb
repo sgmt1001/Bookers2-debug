001419cb145627480c282ad874a387bd37388bf5
EC2 Default User <ec2-user@ip-172-31-59-249.ec2.internal>
2021-07-03 10:33:47 +0000

    task3 done

---
+++ bookers2-debug/app/controllers/book_comments_controller.rb
@@ -0,0 +1,28 @@
+class BookCommentsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    book = Book.find(params[:book_id])
+    comment = BookComment.new(book_comment_params)
+    comment.user_id = current_user.id
+    comment.book_id = book.id
+    comment.save
+    if comment.save
+      redirect_to book_path(book)
+    else
+      redirect_to books_path
+    end
+  end
+
+  def destroy
+    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+    redirect_to book_path(params[:book_id])
+  end
+
+  private
+
+  def book_comment_params
+    params.require(:book_comment).permit(:book_comment)
+  end
+
+end
--- bookers2-debug/config/routes.rb
+++ bookers2-debug/config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to:'homes#top'
-  resources :books
+  
+  resources :books do
+    resources :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
+  
   get 'home/about' => 'homes#about'
   resources :users,only: [:show,:index,:edit,:update]
 
