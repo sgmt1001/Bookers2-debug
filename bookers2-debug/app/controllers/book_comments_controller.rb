008a290c77d3d0471fd9b6efb154086e96e5443c
EC2 Default User <ec2-user@ip-172-31-59-249.ec2.internal>
2021-07-10 07:27:20 +0000

    task6

--- bookers2-debug/app/controllers/book_comments_controller.rb
+++ bookers2-debug/app/controllers/book_comments_controller.rb
@@ -2,21 +2,16 @@ class BookCommentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = BookComment.new(book_comment_params)
     comment.user_id = current_user.id
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    if comment.save
-      redirect_to book_path(book)
-    else
-      redirect_to book_path(book) ,notice: "コメントを入力してください"
-    end
   end
 
   def destroy
+    @book = Book.find(params[:book_id])
     BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to book_path(params[:book_id])
   end
 
   private
